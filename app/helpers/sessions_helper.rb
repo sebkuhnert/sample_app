fda8804f7231957f0445078934f8ea774bd7d29e
sebkuhnert <kuhnertsebastian@gmail.com>
2014-03-30 22:52:18 +0200

    another save

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -16,8 +16,19 @@ module SessionsHelper
   end
 
   def current_user
-    remember_token = User.hash(cookies[:remember_token])
+    remember_token  = User.hash(cookies[:remember_token])
     @current_user ||= User.find_by(remember_token: remember_token)
+  end
+
+  def current_user?(user)
+    user == current_user
+  end
+
+  def signed_in_user
+    unless signed_in?
+      store_location
+      redirect_to signin_url, notice: "Please sign in."
+    end
   end
 
   def sign_out
@@ -25,5 +36,14 @@ module SessionsHelper
                                   User.hash(User.new_remember_token))
     cookies.delete(:remember_token)
     self.current_user = nil
+  end
+
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    session.delete(:return_to)
+  end
+
+  def store_location
+    session[:return_to] = request.url if request.get?
   end
 end
